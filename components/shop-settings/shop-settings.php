5fc682a551e6c9aa3b31b8148c1dc995cf140c22
AeShevch <Neron610>
2020-03-26 12:00:23 +0300

    fix(shop-settings): fixes php notices

--- components/shop-settings/shop-settings.php
+++ components/shop-settings/shop-settings.php
@@ -10,25 +10,27 @@ $settingsArray[0]['link'] = '';
 $settingsArray[0]['content'][0]['title'] = 'Язык';
 $settingsArray[0]['content'][0]['type'] = 'lang';
 $settingsArray[0]['content'][0]['items'] = unserialize(stripcslashes(MG::getSetting('multiLang')));
-console_log($settingsArray[0]['content'][0]['items']);
-foreach ($settingsArray[0]['content'][0]['items'] as $key => $item) {
-    if (!$item['active'] === 'true' ||!$item['enabled'] === 'true') continue;
-    $settingsArray[0]['content'][0]['items'][$key]['title'] = $item['full'];
-}
 
-if (MG::getSetting('printCurrencySelector') == 'true') {
-    $settingsArray[0]['content'][1]['title'] = 'Валюта';
-    $settingsArray[0]['content'][1]['type'] = 'curr';
-    $currencies = MG::getSetting('currencyShort');
-    foreach ($currencies as $key => $item) {
-        $settingsArray[0]['content'][1]['items'][$key]['title'] = $currencies[$key];
-        $settingsArray[0]['content'][1]['items'][$key]['activity'] = $currencies[$key] === $data['currency'] ? true : false;
-        $settingsArray[0]['content'][1]['items'][$key]['class'] = 'js-curr-select';
+if (!empty($settingsArray[0]['content'][0]['items'])) {
+    foreach ($settingsArray[0]['content'][0]['items'] as $key => $item) {
+        if (!$item['active'] === 'true' || !$item['enabled'] === 'true') continue;
+        $settingsArray[0]['content'][0]['items'][$key]['title'] = $item['full'];
     }
-}
 
-component(
-  'mega-menu',
-  ['id' => 'shop-settings', 'items' => $settingsArray],
-  'simple-menu'
-);
+    if (MG::getSetting('printCurrencySelector') == 'true') {
+        $settingsArray[0]['content'][1]['title'] = 'Валюта';
+        $settingsArray[0]['content'][1]['type'] = 'curr';
+        $currencies = MG::getSetting('currencyShort');
+        foreach ($currencies as $key => $item) {
+            $settingsArray[0]['content'][1]['items'][$key]['title'] = $currencies[$key];
+            $settingsArray[0]['content'][1]['items'][$key]['activity'] = $currencies[$key] === $data['currency'] ? true : false;
+            $settingsArray[0]['content'][1]['items'][$key]['class'] = 'js-curr-select';
+        }
+    }
+
+    component(
+      'mega-menu',
+      ['id' => 'shop-settings', 'items' => $settingsArray],
+      'simple-menu'
+    );
+}
