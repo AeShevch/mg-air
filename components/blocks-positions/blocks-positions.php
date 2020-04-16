08556f683074f4cf5a18145b21fe73181c5d22f6
AeShevch <Neron610>
2020-04-16 12:36:58 +0300

    feat(block-positions): adds the ability to change blocks order

--- components/blocks-positions/blocks-positions.php
+++ components/blocks-positions/blocks-positions.php
@@ -15,9 +15,9 @@ $sections = [
     'activity' => MG::get('templateParams')['CATEGORIES_GRID']['activity'],
     'position' => !empty(MG::get('templateParams')['CATEGORIES_GRID']['position']) ? intval(MG::get('templateParams')['CATEGORIES_GRID']['position']) : '',
   ],
-  'a-groups' => [
-    'activity' => MG::get('templateParams')['PRODUCT_GROUPS']['activity'],
-    'position' => !empty(MG::get('templateParams')['PRODUCT_GROUPS']['position']) ? intval(MG::get('templateParams')['PRODUCT_GROUPS']['position']) : '',
+  'a-product-tabs' => [
+    'activity' => MG::get('templateParams')['PRODUCT_TABS']['activity'],
+    'position' => !empty(MG::get('templateParams')['PRODUCT_TABS']['position']) ? intval(MG::get('templateParams')['PRODUCT_TABS']['position']) : '',
   ],
   'a-brands-grid' => [
     'activity' => MG::get('templateParams')['BRANDS_GRID']['activity'] === 'true' && class_exists('Brands') ? 'true' : 'false',
@@ -30,6 +30,22 @@ $sections = [
   'a-footer' => [
     'activity' => MG::get('templateParams')['FOOTER']['activity'],
     'position' => !empty(MG::get('templateParams')['FOOTER']['position']) ? intval(MG::get('templateParams')['FOOTER']['position']) : '',
+  ],
+  'a-product-video_id_1' => [
+    'activity' => MG::get('templateParams')['VIDEO_PRODUCT_1']['activity'],
+    'position' => !empty(MG::get('templateParams')['VIDEO_PRODUCT_1']['position']) ? intval(MG::get('templateParams')['VIDEO_PRODUCT_1']['position']) : '',
+  ],
+  'a-product-video_id_2' => [
+    'activity' => MG::get('templateParams')['VIDEO_PRODUCT_2']['activity'],
+    'position' => !empty(MG::get('templateParams')['VIDEO_PRODUCT_2']['position']) ? intval(MG::get('templateParams')['VIDEO_PRODUCT_2']['position']) : '',
+  ],
+  'a-triggers' => [
+    'activity' => MG::get('templateParams')['TRIGGERS']['activity'],
+    'position' => !empty(MG::get('templateParams')['TRIGGERS']['position']) ? intval(MG::get('templateParams')['TRIGGERS']['position']) : '',
+  ],
+  'a-groups' => [
+    'activity' => MG::get('templateParams')['GROUPS']['activity'],
+    'position' => !empty(MG::get('templateParams')['GROUPS']['position']) ? intval(MG::get('templateParams')['GROUPS']['position']) : '',
   ],
 ];
 ?>
